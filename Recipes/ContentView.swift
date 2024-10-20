d35ba6b866a21e85133de5e4b51f19ac4c9c1be6
Norah240 <nalshehri24@twq.idserve.net>
2024-10-20 16:36:40 +0300

    Initial Commit

---
+++ Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipes
+//
+//  Created by Norah mo on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Norah mo on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
